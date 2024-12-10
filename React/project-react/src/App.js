003bb42b045ea8e9ac7edf86649b6d7fc0cfd00b
milske <millanerg@gmail.com>
2024-12-10 07:17:19 +0200

    react

---
+++ React/project-react/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import MovieSearch from "./Components/MovieSearch";
+
+/* const App = () => {
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center mb-4">Movies</h1>
+      <MovieSearch />
+    </div>
+  );
+};
+
+export default App; ¨*/
